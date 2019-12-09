3fc1eebf3564132dcc25ba8e8412230ab20ca527
zenghuangwei <zenghuangweimaya@qq.com>
2019-12-09 18:14:37 +0800

    finish city scoll

--- src/pages/City/City.vue
+++ src/pages/City/City.vue
@@ -2,17 +2,24 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-list></city-list>
+        <city-alphabet></city-alphabet>
     </div>
 </template>
 
 <script>
     import CityHeader from './components/CityHeader'
-    import CitySearch from './components/CitySearch'
+    import CitySearch from './components/CitySearch'    
+    import CityList from './components/CityList'  
+    import CityAlphabet from './components/Alphabet' 
+
     export default {
         name:'City',
         components:{
             CityHeader,
             CitySearch,
+            CityList,
+            CityAlphabet
         }
     }
 </script>
