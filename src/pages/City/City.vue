9f21fdaf370579e5516608f551aba23caf9f1142
zenghuangwei <zenghuangweimaya@qq.com>
2019-12-09 15:05:40 +0800

    city search

--- src/pages/City/City.vue
+++ src/pages/City/City.vue
@@ -1,17 +1,22 @@
 <template>
-    <city-header></city-header>
+    <div>
+        <city-header></city-header>
+        <city-search></city-search>
+    </div>
 </template>
 
 <script>
     import CityHeader from './components/CityHeader'
+    import CitySearch from './components/CitySearch'
     export default {
         name:'City',
         components:{
-            CityHeader
+            CityHeader,
+            CitySearch,
         }
     }
 </script>
 
 <style lang="stylus" scoped>
-
+    
 </style>
